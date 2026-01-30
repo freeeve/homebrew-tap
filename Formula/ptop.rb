b51296178225727a1c1673780b59863800a57a00
Eve Freeman <eve.freema@gmail.com>
2026-01-29 20:14:54 -0500

    feat: add ptop formula v0.1.0

---
+++ Formula/ptop.rb
@@ -0,0 +1,43 @@
+class Ptop < Formula
+  desc "Network latency monitor with terminal UI - htop for ping"
+  homepage "https://github.com/freeeve/ptop"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/freeeve/ptop/releases/download/v#{version}/ptop-macos-aarch64.tar.gz"
+      sha256 "3ac8f5b8b70b14f845c9cb5dd683d7db4cb6da3227bef2872d4e49a937d2f292"
+    end
+    on_intel do
+      url "https://github.com/freeeve/ptop/releases/download/v#{version}/ptop-macos-x86_64.tar.gz"
+      sha256 "23bc232919ce6ef8ae8a3da2c78c916b0fb883c2b6effd20498d7f3ab06672ac"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/freeeve/ptop/releases/download/v#{version}/ptop-linux-x86_64.tar.gz"
+      sha256 "69e4f48c668d369185a471086082150e2fc22e721792d3220c6821288262d0b8"
+    end
+  end
+
+  def install
+    bin.install "ptop"
+  end
+
+  def caveats
+    <<~EOS
+      ptop requires elevated privileges to send ICMP packets.
+      Run with sudo:
+        sudo ptop
+
+      Or on Linux, set capabilities:
+        sudo setcap cap_net_raw=ep #{bin}/ptop
+    EOS
+  end
+
+  test do
+    assert_match "ptop #{version}", shell_output("#{bin}/ptop --version")
+  end
+end
